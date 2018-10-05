53dc1340fa4ab11273f67144f05345e65fe7b1a6
Nikita Avvakumov <na.avvakumov@gmail.com>
2018-10-06 00:34:36 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,57 +1,38 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
-  var prost = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]; 
-  var nul = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; 
-  var schet = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; 
-  let a=base;
-  let del=0;
-  for(i=0;i<54;i++)
+  let prost = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]; 
+  let nul = [];
+  let zeroes = [];
+  let rez = [];
+  for (let i = 0; i < prost.length; i++)
   { 
-    if(a%prost[i]==0)
-    { 
-      a=a/prost[i]; 
-      nul[i]+=1; 
-      i--; 
+    let count = 0;
+    if(base == 1) break;
+    while (base%prost[i]==0)
+    {
+      base/=prost[i];
+      count++
+      if (base%prost[i] != 0){
+        nul.push([prost[i],count]);
+      }
     } 
   } 
-  for(j=2; j<=number;j++)
+  for (let j = 0; j<nul.length; j++) 
   {
-    for(i=0;i<54;i++)
+    let zc = 0;
+    let a = 1;
+    while (nul[j][0]**a <= number)
     {
-      if (nul[i]>0)
-      {
-        
-       del=j;
-        while(del%prost[i]==0)
-        {
-          del=del/prost[i];
-          schet[i]+=1;
-        }
-      }
+      zc+=Math.floor(number/nul[j][0]**a++);
     }
+    zeroes.push(zc);
   }
-  for(i=0;i<54;i++)
+  
+  for (let i = 0; i<nul.length; i++)
   {
-    if(nul[i]>0)
-    {
-      schet[i]=schet[i]/nul[i];
-    }
+     rez.push(Math.floor(zeroes[i]/nul[i][1]));
   }
-  let max=0;
-  for(i=0;i<54;i++)
-  {
-    if(schet[i]>max)
-    {
-      max=schet[i];
-    }
-  }
-  let min=max;
-  for(i=0;i<54;i++)
-  {
-    if(schet[i]<min && schet[i]>0)
-    {
-      min=schet[i];
-    }
-  }
-  return Math.floor(min);
+  rez.sort((a,b)=> a-b);
+  return rez[0];
 }
+  
